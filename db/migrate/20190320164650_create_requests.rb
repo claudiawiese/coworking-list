f206745fe66d2fe21bb4fee98d9bea6a8e86f419
Claudia Wiese <claudia.wiese@sciencespo.fr>
2019-03-20 17:49:33 +0100

    changed database structure

--- db/migrate/20190320164650_create_requests.rb
+++ db/migrate/20190320164650_create_requests.rb
@@ -2,8 +2,12 @@ class CreateRequests < ActiveRecord::Migration[5.2]
   def change
     create_table :requests do |t|
       t.datetime :date
+      t.string :client_first_name
+      t.string :client_last_name
+      t.string :email
+      t.string :phone
+      t.text :bio
       t.string :status
-      t.references :client, foreign_key: true
 
       t.timestamps
     end
